031fe9a9f76afd92739cbff66b5351c95e820c42
Dhirajkumar122 <105111062+Dhirajkumar122@users.noreply.github.com>
2023-10-14 09:53:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, { text: task, completed: false }]);
+      setTask('');
+    }
+  };
+
+  const toggleComplete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.completed ? 'completed' : ''}>
+            {todo.text}
+            <div>
+              <button onClick={() => toggleComplete(index)}>
+                {todo.completed ? 'Undo' : 'Complete'}
+              </button>
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
